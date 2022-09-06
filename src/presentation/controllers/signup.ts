4832b24f927e3eaebba6fa621f7b9c651e4cf636
Roger-Franco <rogerfrancogalt@gmail.com>
2022-09-05 21:50:10 -0400

    feat: ensure signupController return 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,10 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
+export class SignupController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
